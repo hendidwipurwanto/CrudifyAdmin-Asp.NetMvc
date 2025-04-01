12d1b37f17e7ed0ba2ffc71b1c03fc680d3a2a08
hendidwipurwanto <hendi.purwanto@gustodian.com>
2025-04-01 12:03:28 +0700

    added layering domain, infrastructure and  add connectionstrig

--- Hendi_Dwi_Purwanto/Web/Program.cs
+++ Hendi_Dwi_Purwanto/Web/Program.cs
@@ -1,6 +1,7 @@
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Web.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,6 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
